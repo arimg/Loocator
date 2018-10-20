15ad5e3eb15351b1de1b88ff90ca85a87dde4cf6
Arielle Goldberg <ariellegoldberg@dhcp-ccc-3646.redrover.cornell.edu>
2018-10-20 16:27:34 -0400

    added element

--- loocator/src/app/here-map.service.ts
+++ loocator/src/app/here-map.service.ts
@@ -1,20 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ViewChild, ElementRef } from '@angular/core';
 
 declare var H: any;
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HereMapService {
 
   public platform: any;
   public geocoder: any;
+  public map: any;
+
+  @ViewChild('mapContainer') targetElement: ElementRef; 
 
   constructor() {
     this.platform = new H.service.Platform({
         "app_id": "fag1SEc67LrF8clS7SuE",
         "app_code": "oxZZk80QAOMaJa4rEjp7Gg"
     });
+    const defaultLayers = this.platform.createDefaultLayers();
+    this.map = new H.Map(
+      this.targetElement,
+      defaultLayers.normal.map
+      ({
+        zoom: 10,
+        center: { lat: 52.51, lng: 13.4 }
+        }));
+    
 
     this.geocoder = this.platform.getGeocodingService();
    }
